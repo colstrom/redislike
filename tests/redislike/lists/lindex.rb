9b9f3f899a2635a01529edf0160ed95e4512072e
Chris Olstrom <chris@olstrom.com>
2015-04-07 07:06:01 -0700

    More readaable tests.

--- tests/redislike/lists/lindex.rb
+++ tests/redislike/lists/lindex.rb
@@ -1,22 +1,31 @@
 context 'LINDEX' do
   setup do
     @db = Moneta.new :Memory
-    @db.lpush 'test:lindex', 'foo'
-    @db.lpush 'test:lindex', 'bar'
-    @db.lpush 'test:lindex', 'baz'
   end
 
-  test 'when list does not exist' do
-    assert_equal nil, @db.lindex('test:nx', 1)
+  given 'an empty list' do
+    should 'return nothing' do
+      assert_equal nil, @db.lindex('test:nx', 1)
+    end
   end
 
-  context 'when list exists' do
-    test 'index in range' do
-      assert_equal 'bar', @db.lindex('test:lindex', 1)
+  given 'a list with 3 items' do
+    setup do
+      @db.lpush 'test:lindex', 'foo'
+      @db.lpush 'test:lindex', 'bar'
+      @db.lpush 'test:lindex', 'baz'
     end
 
-    test 'index out of range' do
-      assert_equal nil, @db.lindex('test:lindex', 999)
+    context 'a request for an index within range' do
+      should 'return the item at that index' do
+        assert_equal 'bar', @db.lindex('test:lindex', 1)
+      end
+    end
+
+    context 'a request for an index out of range' do
+      should 'return nothing' do
+        assert_equal nil, @db.lindex('test:lindex', 999)
+      end
     end
   end
 
